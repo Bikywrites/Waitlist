ac40b3a43f176467ce5718ed2a2dda378b3987a7
Paul Chidiadi <paulchidiadi@gmail.com>
2024-05-29 07:28:16 +0100

    Create landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,15 @@ module.exports = {
     extend: {
       colors: {
         blue: "#006FDA",
+        lighterBlue: "#2d93f1",
         yellowish: "#FFCC00",
+        textblack: "#475467",
+        textheading: "#101828",
+      },
+      fontFamily: {
+        jakata: ['"Plus Jakarta Sans"', "sans-serif"],
+        inter: ['"Inter"', "sans-serif"],
+        ubuntu: ['"Ubuntu"', "sans-serif"],
       },
     },
   },
